13582a6a1d6105df59648fb5a4e2cc3a53a6ed96
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-11-12 16:04:59 +0200

    lesson1

---
+++ resources/views/product.blade.php
@@ -0,0 +1,71 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Интернет Магазин: Товар</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/starter-template.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="https://internet-shop.tmweb.ru">Интернет Магазин</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li ><a href="https://internet-shop.tmweb.ru">Все товары</a></li>
+                <li ><a href="https://internet-shop.tmweb.ru/categories">Категории</a>
+                </li>
+                <li ><a href="https://internet-shop.tmweb.ru/basket">В корзину</a></li>
+                <li><a href="https://internet-shop.tmweb.ru/reset">Сбросить проект в начальное состояние</a></li>
+                <li><a href="https://internet-shop.tmweb.ru/locale/en">en</a></li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="https://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
+                        <li><a href="https://internet-shop.tmweb.ru/currency/USD">$</a></li>
+                        <li><a href="https://internet-shop.tmweb.ru/currency/EUR">€</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="https://internet-shop.tmweb.ru/login">Войти</a></li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="starter-template">
+        <h1>iPhone X 64GB</h1>
+        <h2>Мобильные телефоны</h2>
+        <p>Цена: <b>71990 ₽</b></p>
+        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
+        <p>Отличный продвинутый телефон с памятью на 64 gb</p>
+
+
+        <span>product.not_available</span>
+        <br>
+        <span>Сообщить мне, когда товар появится в наличии:</span>
+        <div class="warning">
+        </div>
+        <form method="POST" action="https://internet-shop.tmweb.ru/subscription/1">
+            <input type="hidden" name="_token" value="7IzSj5ij6LsH3lbOS2YzEgUfrH4aWvCcTtxWjIkv">            <input type="text" name="email"></input>
+            <button type="submit">Отправить</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/categories', function () {
+    return view('categories');
+});
+
+Route::get('/mobiles/iphone_x_64', function () {
+    return view('product');
 });
