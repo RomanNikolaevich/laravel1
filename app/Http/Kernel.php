f6bce0a45d97623cce1fa91458c14fa456557c7c
RomanNikolaevich <85992849+RomanNikolaevich@users.noreply.github.com>
2022-12-16 04:58:38 +0200

    feat/currency CurrencyController, CurrencyService and konsole test (#8)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\ForceJsonResponse;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -67,5 +68,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'set_locale' => \App\Http\Middleware\SetLocale::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,50 +16,53 @@ Auth::routes([
     'verify'  => false,
 ]);
 
+Route::get('locale/{locale}', [MainController::class, 'changeLocale'])->name('locale');
+
 Route::get('reset', [ResetController::class, 'reset'])->name('reset');
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('get-logout');
 
-Route::middleware(['auth'])-> group(function (){
-    Route::group([
-        'prefix' => 'person',
-        'namespace' => 'Person',
-        'as' => 'person.',
-    ], static function () {
-        Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
-        Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
-    });
-
-    Route::group([
-        'prefix'     => 'admin',
-    ], static function () {
-        Route::group(['middleware' => 'is_admin'], static function () {
-            Route::get('/orders', [OrderController::class, 'index'])->name('home');
+Route::middleware(['set_locale'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::group([
+            'prefix'    => 'person',
+            'namespace' => 'Person',
+            'as'        => 'person.',
+        ], static function () {
+            Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
             Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
         });
 
-        Route::resource('categories', CategoryController::class);
-        Route::resource('products', ProductController::class);
+        Route::group([
+            'prefix' => 'admin',
+        ], static function () {
+            Route::group(['middleware' => 'is_admin'], static function () {
+                Route::get('/orders', [OrderController::class, 'index'])->name('home');
+                Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+            });
+
+            Route::resource('categories', CategoryController::class);
+            Route::resource('products', ProductController::class);
+        });
     });
+
+    Route::get('/', [MainController::class, 'index'])->name('index');
+    Route::get('/categories', [MainController::class, 'categories'])->name('categories');
+
+    Route::group([
+        'middleware' => 'basket_not_empty',
+        'prefix'     => 'basket',
+    ], static function () {
+        Route::get('/', [BasketController::class, 'basket'])->name('basket');
+        Route::get('/place', [BasketController::class, 'basketPlace'])->name('basket-place');
+
+        Route::post('/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
+        Route::post('/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
+    });
+
+    Route::post('/basket/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
+
+    Route::get('/{category}', [MainController::class, 'category'])->name('category');
+    Route::get('/{category}/{product?}', [MainController::class, 'product'])
+        ->name('product');
 });
-
-
-Route::get('/', [MainController::class, 'index'])->name('index');
-Route::get('/categories', [MainController::class, 'categories'])->name('categories');
-
-Route::group([
-    'middleware' => 'basket_not_empty',
-    'prefix'     => 'basket',
-], static function () {
-    Route::get('/', [BasketController::class, 'basket'])->name('basket');
-    Route::get('/place', [BasketController::class, 'basketPlace'])->name('basket-place');
-
-    Route::post('/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
-    Route::post('/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
-});
-
-Route::post('/basket/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
-
-Route::get('/{category}', [MainController::class, 'category'])->name('category');
-Route::get('/{category}/{product?}', [MainController::class, 'product'])
-    ->name('product');
