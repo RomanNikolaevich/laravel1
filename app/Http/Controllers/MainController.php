31d511e6396923903009b9224b9001fdd910311c
RomanNikolaevich <85992849+RomanNikolaevich@users.noreply.github.com>
2022-12-16 05:00:18 +0200

    Revert "feat/currency CurrencyController, CurrencyService and konsole test (#8)"
    This reverts commit f6bce0a45d97623cce1fa91458c14fa456557c7c.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,46 +7,29 @@ use App\Models\Product;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\App;
 
 class MainController extends Controller
 {
     public function index():Factory|View|Application
     {
         $products = Product::get();
-
         return view('index', compact('products'));
     }
 
     public function categories():Factory|View|Application
     {
         $categories = Category::get();
-
         return view('categories', compact('categories'));
     }
 
-    public function category($code):Factory|View|Application
+    public function category($code): Factory|View|Application
     {
         $category = Category::where('code', $code)->first();
-
         return view('category', compact('category'));
     }
 
     public function product($category, $product = null):Factory|View|Application//означает, что свойство не обязательное
     {
         return view('product', ['product' => $product]); //второй вариант передачи параметров
-    }
-
-    public function changeLocale($locale):RedirectResponse
-    {
-        $avaibleLocales = ['ua', 'en'];
-        if (!in_array($locale, $avaibleLocales, true)) {
-            $locale = config('app.locale');
-    }
-        session(['locale' => $locale]);
-        App::setLocale($locale);
-
-        return redirect()->back();
     }
 }
