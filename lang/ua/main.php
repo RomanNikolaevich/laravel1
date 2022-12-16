9abcf760356f3107980651439a43aa04c6b68226
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-16 04:55:58 +0200

    feat/currency CurrencyController, CurrencyService and konsole test

---
+++ lang/ua/main.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'online_shop'  => 'Інтернет Магазин',
+    'all_products' => 'Усі товари',
+    'categories'   => 'Категорії',
+    'basket'       => 'Кошик',
+    'reset_config' => 'Скинути проект у початковий стан',
+    'log_in'       => 'Увійти',
+    'admin_panel'  => 'Панель Адміністратора',
+    'my_orders'    => 'Мої замовлення',
+    'log_out'      => 'Вийти',
+    'current_lang' => 'ua',
+    'set_lang'     => 'en',
+];
