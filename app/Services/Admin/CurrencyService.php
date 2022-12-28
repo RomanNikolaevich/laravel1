b5b8096508742838d7a937d94c6296eb6f460e3d
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-28 04:06:38 +0200

    CurrencyService refactoring

--- app/Services/Admin/CurrencyService.php
+++ app/Services/Admin/CurrencyService.php
@@ -8,155 +8,168 @@ use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Facades\Cache;
+use RuntimeException;
 
 class CurrencyService
 {
-	private array $codes;
-	private array|int $ratio;
+    private array $codes;
+    private array|int $ratio;
 
-	public function __construct()
-	{
-		$this->codes = config('currency.codes');
-		$this->ratio = config('currency.ratio');
-	}
+    public function __construct()
+    {
+        $this->codes = config('currency.codes');
+        $this->ratio = config('currency.ratio');
+    }
 
 
-	/**
-	 * Entering exchange rates into our database from an external source
-	 *
-	 * @param Carbon $date
-	 * @return void
-	 * @throws GuzzleException
-	 * @throws \JsonException
-	 */
-	public function updateCurrencies(Carbon $date): void
-	{
-		$currencies = $this->getRatesByApi($date);
+    /**
+     * Entering exchange rates into our database from an external source
+     *
+     * @param Carbon $date
+     * @return void
+     * @throws GuzzleException
+     * @throws \JsonException
+     */
+    public function updateCurrencies(Carbon $date): void
+    {
+        $currencies = $this->getRatesByApi($date);
 
-		foreach ($this->codes as $code) {
-			foreach ($currencies as $currency) {
-				if ($currency['cc'] === $code && !empty($currency['rate'])) {
-					$currencyData = [
-						'code' => $code,
-						'rate' => $currency['rate'] * $this->ratio,
-						'enabled_at' => $date->format('Y-m-d'),
-					];
+        foreach ($this->codes as $code) {
+            foreach ($currencies as $currency) {
+                if ($currency['cc'] === $code && !empty($currency['rate'])) {
+                    $currencyData = [
+                        'code' => $code,
+                        'rate' => $currency['rate'] * $this->ratio,
+                        'enabled_at' => $date->format('Y-m-d'),
+                    ];
 
-					Currency::firstOrCreate($currencyData);
-				}
-			}
-		}
-	}
+                    Currency::firstOrCreate($currencyData);
+                }
+            }
+        }
+    }
 
-	/**
-	 * Getting the exchange rate for a specified date from our database
-	 *
-	 * @param Carbon $date
-	 * @param string $code
-	 * @return float|bool|int|null
-	 * @throws Exception
-	 */
-	public function getCurrencyRateFromDB(Carbon $date, string $code): float|bool|int|null
-	{
-		if (!in_array($code, $this->codes, true)) {
-			throw new Exception('Wrong currency code');
-		}
+    /**
+     * Getting the exchange rate for a specified date from our database
+     *
+     * @param Carbon $date
+     * @param string $code
+     * @return float|bool|int|null
+     */
+    public function getCurrencyRateFromDB(Carbon $date, string $code): float|bool|int|null
+    {
+        $currencyCollection = Currency::where('code', $code)
+            ->where('enabled_at', $date->format('Y-m-d'))
+            ->first();
 
-		$currencyCollection = Currency::where('code', $code)
-									  ->where('enabled_at', $date->format('Y-m-d'))
-									  ->first();
+        if ($currencyCollection === null) {
+            return null;
+        }
 
-		if ($currencyCollection === null) {
-			return null;
-		}
+        $rate = $currencyCollection['rate'] / $this->ratio;
 
-		$rate = $currencyCollection['rate'] / $this->ratio;
+        $secondsToDayEnd = Carbon::tomorrow()->diffInSeconds(Carbon::now());
 
-		return Cache::remember('rate', 60 * 60 * 24, function () use ($rate) {
-			return $rate;
-		});
-	}
+        return Cache::remember('rate_' . $code, $secondsToDayEnd, function () use ($rate) {
+            return $rate;
+        });
+    }
 
-	/**
-	 * Calculation of the price of goods in the specified currency with rounding to a significant figure
-	 *
-	 * @param Carbon $date
-	 * @param string $code
-	 * @param int $id
-	 * @return float|int
-	 * @throws Exception
-	 */
-	public function convertPrice(Carbon $date, string $code, int|float $priceInDefaultCurrency): float|int|null
-	{
-		$priceBeforeRounding = $priceInDefaultCurrency / $this->getCurrencyRateFromDB($date, $code);
-		$precision = config('currency.precision');
-		$convertPrice = $this->numberToSignificant($priceBeforeRounding, $precision);
+    /**
+     * Calculation of the price of goods in the specified currency with rounding to a significant figure
+     *
+     * @param Carbon $date
+     * @param string $code
+     * @param int $id
+     * @return float|int
+     * @throws RuntimeException
+     */
+    public function convertPrice(Carbon $date, string $code, int|float $priceInDefaultCurrency): float|int|null
+    {
+        /** @var string $defaultCode */
+        $defaultCode = config('currency.default_code');
 
-		return $convertPrice;
-	}
+        if ($code === $defaultCode) {
+            return $priceInDefaultCurrency;
+        }
 
-	/**
-	 * This Client is a Guzzle(PHP HTTP client)
-	 *
-	 * @return Client
-	 */
-	private function getClient(): Client
-	{
-		return new Client([
-			'base_uri' => config('currency.api_url'),
-			'timeout' => 2.0,
-			'verify' => false,
-		]);
-	}
+        if (!in_array($code, $this->codes, true)) {
+            $code = $defaultCode;
+        }
 
-	/**
-	 *
-	 *
-	 * @param Carbon $date
-	 * @return array|null
-	 * @throws GuzzleException
-	 * @throws \JsonException
-	 */
-	private function getRatesByApi(Carbon $date): ?array
-	{
-		$client = $this->getClient();
-		$urn = '?&date=' . $date->format('Ymd') . '&json';
-		$response = $client->request('GET', $urn);
+        $rate = $this->getCurrencyRateFromDB($date, $code);
 
-		if ($response->getStatusCode() !== 200) {
-			throw new Exception('There is a problem with currency rate service');
-		}
+        if (!$rate) {
+            return 0;
+        }
 
-		$rateCurrency = json_decode(
-			$response->getBody()->getContents(),
-			true,
-			512,
-			JSON_THROW_ON_ERROR
-		);
+        //return $priceInDefaultCurrency / $rate;
+        return $this->numberToSignificant($priceInDefaultCurrency / $rate) ;
+    }
 
-		if ($rateCurrency === null) {
-			throw new Exception('There is no exchange rate for the specified date: ' . $date);
-		}
+    /**
+     * This Client is a Guzzle(PHP HTTP client)
+     *
+     * @return Client
+     */
+    private function getClient(): Client
+    {
+        return new Client([
+            'base_uri' => config('currency.api_url'),
+            'timeout' => 2.0,
+            'verify' => false,
+        ]);
+    }
 
-		return $rateCurrency;
-	}
+    /**
+     *
+     *
+     * @param Carbon $date
+     * @return array|null
+     * @throws GuzzleException
+     * @throws \JsonException
+     */
+    private function getRatesByApi(Carbon $date): ?array
+    {
+        $client = $this->getClient();
+        $urn = '?&date=' . $date->format('Ymd') . '&json';
+        $response = $client->request('GET', $urn);
 
-	/**
-	 * rounding to significant numbers
-	 *
-	 * @param int|float|null $number
-	 * @param int $precision
-	 * @return float|int|null
-	 */
-	private function numberToSignificant(int|float|null $number, int $precision): float|int|null
-	{
-		if ($number === 0) {
-			return null;
-		}
+        if ($response->getStatusCode() !== 200) {
+            throw new Exception('There is a problem with currency rate service');
+        }
 
-		$exponent = floor(log10(abs($number)) + 1);
-		$significant = round(($number / (10 ** $exponent)) * (10 ** $precision)) / (10 ** $precision);
+        $rateCurrency = json_decode(
+            $response->getBody()->getContents(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
 
-		return $significant * (10 ** $exponent);
-	}
+        if ($rateCurrency === null) {
+            throw new Exception('There is no exchange rate for the specified date: ' . $date);
+        }
+
+        return $rateCurrency;
+    }
+
+    /**
+     * rounding to significant numbers
+     *
+     * @param int|float|null $number
+     * @param int $precision
+     * @return float|int|null
+     */
+    private function numberToSignificant(int|float|null $number): float|int|null
+    {
+        if ($number === 0) {
+            return null;
+        }
+
+        $precision = config('currency.precision');
+        $exponent = floor(log10(abs($number)) + 1);
+        $significant = round(($number / (10 ** $exponent)) * (10 ** $precision)) / (10 ** $precision);
+
+        return round($significant * (10 ** $exponent), 2);
+    }
 }
